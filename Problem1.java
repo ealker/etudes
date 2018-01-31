e5be4170dde707bba8f214b36542061121cf7e9c
ealker <elliot@anamatica.com>
2018-01-31 13:11:26 +0000

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -5,6 +5,7 @@
  * Find the sum of all the multiples of 3 or 5 below 1000.
  *
  * Answer: 233168
+ *
  */
 
 public class Problem1 {
@@ -14,7 +15,7 @@ public class Problem1 {
         int result = 0;
 
         for (int i = 0; i < 1000; i++){
-            if (((i % 3) == 0) || ((i % 5) == 0 )){
+            if (((i % 3) == 0) || ((i % 5) == 0 )){ 
                 result += i;
             }
         }
