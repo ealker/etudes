a61bcae4eb58ce7a71e466d1512bf7751c172838
Elliot Alker <2823357+ealker@users.noreply.github.com>
2023-06-21 19:24:33 +0100

    Removed old files, small solution now up. Needs different regex.

--- python-speech-counter/wordcount.py
+++ python-speech-counter/wordcount.py
@@ -1,29 +1,14 @@
 # Program to read in some text and count the instances of individual words. 
+from collections import Counter
+import re
 
-with open('speech.txt', 'r') as file:
-  speech = file.read()
-
-
-
-speech = open('speech.txt', 'r').read()
-
-
-
-print speech.split('\n')
-
-
-
-for line in speech:
-    lineWords = line.split()
-
-
-    for word in lineWords:
-         print word
-
-
-
-
+words = re.findall(r'\w+', open('speech.txt').read().lower())
+# words = re.split('\d+', open('speech.txt').read().lower())
+for key, value in Counter(words).most_common():
+    print(key, value)
 
 #Strip punctuation.
+
 #Lower Case - Group all words together.
+
 #Count function.
