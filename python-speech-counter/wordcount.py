98c721364badc4516ecc9eefb414d369e7fac3ca
Elliot Alker <2823357+ealker@users.noreply.github.com>
2023-06-21 19:25:31 +0100

    Removed comments.

--- python-speech-counter/wordcount.py
+++ python-speech-counter/wordcount.py
@@ -6,9 +6,3 @@ words = re.findall(r'\w+', open('speech.txt').read().lower())
 # words = re.split('\d+', open('speech.txt').read().lower())
 for key, value in Counter(words).most_common():
     print(key, value)
-
-#Strip punctuation.
-
-#Lower Case - Group all words together.
-
-#Count function.
